6f3aad4eeeb31fae1b5f05f68a4b45f1112094b7
Ksawery Pasikowski <ksawerypasikowski@gmail.com>
2017-03-19 17:37:01 +0100

    Ex 5 done

--- projekt_2/zadanie5.m
+++ projekt_2/zadanie5.m
@@ -1,14 +1,50 @@
 parametry;
 
-z(1 : Kk) = 0;
+dirPathFigures = 'wykresy_figury/zad5';
+dirPathTxt = 'wykresy_pliki/zad5';
+mkDirectory(dirPathFigures);
+mkDirectory(dirPathTxt);
 
-% z1(1 : 200) = 0;
-% z1(201 : Kk) = 1;
+z(1 : 200) = 0;
+z(201 : Kk) = 1;
 
-% x = (1 : 1 : Kk);
-% f = 500;
-% fs = 8000;
-% z2 = sin(2*pi*f/fs*x);
+D = 200;
+Dz = 200;
+N = D;
+Nu = D;
+lambdas = [1 10 50];
+E1s = zeros(1, length(lambdas));
+E2s = zeros(1, length(lambdas));
 
-[y1, u1, E1] = policzDMC(200, 200, 200, 200, 10, Kk, z, 0);
-% [y2, u2, E2] = policzDMC(200, 200, 200, 200, 10, Kk, z1, 1);
+for i = 1 : length(lambdas)
+    
+    lambda = lambdas(i);
+    [y1, u1, E1] = policzDMC(D, Dz, N, Nu, lambda, Kk, z, 0);
+    [y2, u2, E2] = policzDMC(D, Dz, N, Nu, lambda, Kk, z, 1);
+    
+    zapiszDoPliku([dirPathTxt '/wyjscie_bez_pom_lambda_'  num2str(lambdas(i)) '.txt'], y1);
+    zapiszDoPliku([dirPathTxt '/sterowanie_bez_pom_lambda_'  num2str(lambdas(i)) '.txt'], u1);
+    
+    zapiszDoPliku([dirPathTxt '/wyjscie_pom_lambda_'  num2str(lambdas(i)) '.txt'], y2);
+    zapiszDoPliku([dirPathTxt '/sterowanie_pom_lambda_'  num2str(lambdas(i)) '.txt'], u2);
+    
+    figure;
+    subplot(2,1,1);
+    rysujWykres((1 : length(y1)), y1, -1, 'k', 'y', '', ['Wyjscie obiektu z mierzonym zakloceniem i bez, lambda = ' num2str(lambdas(i))]);
+    hold on
+    rysujWykres((1 : length(y2)), y2, -1, 'k', 'y', '', ['Wyjscie obiektu z mierzonym zakloceniem i bez, lambda = ' num2str(lambdas(i))]);
+
+    subplot(2,1,2);
+    rysujWykres((1 : length(u1)), u1, -1, 'k', 'u', '', ['Sterowanie z mierzonym zakloceniem i bez ' num2str(lambdas(i))]);
+    hold on
+    rysujWykres((1 : length(u2)), u2, -1, 'k', 'u', '', ['Sterowanie z mierzonym zakloceniem i bez ' num2str(lambdas(i))]);
+    
+    legend('bez pomiaru zaklocenia', 'z pomiarem zaklocenia');
+    savefig([dirPathFigures '/sterowanie_wyjscie_lambda_ ' num2str(lambdas(i)) '.fig']);
+    
+    E1s(i) = E1;
+    E2s(i) = E2;
+end
+
+zapiszDoPliku([dirPathTxt '/bledy_bez_pom.txt'], E1s);
+zapiszDoPliku([dirPathTxt '/bledy_pom.txt'], E2s);
---
+++ projekt_2/zadanie6.m
@@ -0,0 +1,6 @@
+parametry;
+
+x = (1 : 1 : Kk);
+f = 500;
+fs = 8000;
+z2 = sin(2*pi*f/fs*x);
